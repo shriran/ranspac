55832dfa26b6ec1d3e278117da625f331b78d88c
shriran <89437251+shriran@users.noreply.github.com>
2025-07-01 10:04:36 +0000

    Add .env file and update App.js with Hello World changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <h1>hello world</h1>; (
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello World</h1>
         </p>
         <a
           className="App-link"
